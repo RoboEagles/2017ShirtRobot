63bb060c45223c597de86a1706bf9bdae3ff697a
bstevens <bstevens@FWHS-454403-LT.fwps.ednet>
2016-12-06 15:54:16 -0800

    Started adding subsystem methods to drive command

--- shirtBotJon/src/org/usfirst/frc0/shirtBotJon/commands/drive.java
+++ shirtBotJon/src/org/usfirst/frc0/shirtBotJon/commands/drive.java
@@ -39,24 +39,29 @@ public class drive extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	System.out.println("Starting to drive");
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	
+    	Robot.driveTrain.drive();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
+    	
         return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	System.out.println("Ending drive!");
+    	Robot.driveTrain.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.driveTrain.destroyEverything();
     }
 }
