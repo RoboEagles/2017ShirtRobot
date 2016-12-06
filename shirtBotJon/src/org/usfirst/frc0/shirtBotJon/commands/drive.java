398b8f99aa37c55601ad8401cfe6e422ddbf5a2d
bstevens <bstevens@FWHS-454403-LT.fwps.ednet>
2016-12-06 15:29:09 -0800

    Added Z input to mecanumDrive
    Added comment to Robot

--- shirtBotJon/src/org/usfirst/frc0/shirtBotJon/commands/drive.java
+++ shirtBotJon/src/org/usfirst/frc0/shirtBotJon/commands/drive.java
@@ -13,6 +13,7 @@ package org.usfirst.frc0.shirtBotJon.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc0.shirtBotJon.Robot;
+import org.usfirst.frc0.shirtBotJon.subsystems.driveTrain;
 
 /**
  *
@@ -42,6 +43,7 @@ public class drive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
